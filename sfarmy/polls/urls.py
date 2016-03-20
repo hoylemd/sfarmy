edd03e6a4011973c5f1eb73705f98a20af8f7873
Michael D. Hoyle <hoylemd@gmail.com>
2016-03-20 17:54:23 -0400

    Convert Results

--- sfarmy/polls/urls.py
+++ sfarmy/polls/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
         views.DetailView.as_view(),
         name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$',
+        views.ResultsView.as_view(),
+        name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
