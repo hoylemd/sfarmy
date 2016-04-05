e40dcef1e82f50b04b4d2c73b562bdc8996cf0cd
Michael D. Hoyle <hoylemd@gmail.com>
2016-04-05 13:40:38 -0400

    use callables in url()

--- sfarmy/base/urls.py
+++ sfarmy/base/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 
@@ -7,6 +8,6 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^login/$', login),
+    url(r'^logout/$', logout),
 ] + staticfiles_urlpatterns()
