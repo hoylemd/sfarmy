ad8980d1d379ce4ec5c6e2af49f45f0c762621e0
Michael D. Hoyle <hoylemd@gmail.com>
2016-04-05 13:58:18 -0400

    Add choice editing to question admin

--- sfarmy/polls/admin.py
+++ sfarmy/polls/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    model = Question
+    inlines = [
+        ChoiceInline
+    ]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
