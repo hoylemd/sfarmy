9a70bf68f70eb8876206f42cfb38bb3a0fa284c8
Michael D. Hoyle <hoylemd@gmail.com>
2016-04-05 14:08:55 -0400

    Add owners to polls

--- sfarmy/polls/models.py
+++ sfarmy/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -7,6 +8,10 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              null=True,
+                              blank=True,
+                              on_delete=models.SET_NULL)
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
