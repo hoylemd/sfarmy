45308d29b14fba6dd34eb69f50a6fd9681752d50
Michael D. Hoyle <hoylemd@gmail.com>
2016-04-24 13:56:09 -0400

    add skeleton of question view tests

---
+++ sfarmy/polls/tests/test_question_view.py
@@ -0,0 +1,23 @@
+# from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+
+def create_question():
+    pass
+
+
+class QuestionViewTests(TestCase):
+    def test_index_view__no_questions(self):
+        pass
+
+    def test_index_view__future_question(self):
+        pass
+
+    def test_index_view__past_question(self):
+        pass
+
+    def test_index_view__past_and_future_questions(self):
+        pass
+
+    def test_index_view__2_past_questions(self):
+        pass
