4aefa0ea6c121ce0c3de6f583327d5b687c96578
Michael D. Hoyle <hoylemd@gmail.com>
2016-04-24 13:44:26 -0400

    more tests for recently published

--- sfarmy/polls/tests.py
+++ sfarmy/polls/tests.py
@@ -16,3 +16,21 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently__2_days_ago(self):
+        """
+        should be false for >1 day old questions
+        """
+
+        time = timezone.now() - datetime.timedelta(days=2)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently__12_hours_ago(self):
+        """
+        should be true for < 1 day old questions
+        """
+
+        time = timezone.now() - datetime.timedelta(hours=12)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), True)
