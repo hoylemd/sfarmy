aa7dda0222450970bbddfdfa52f0ce41ab5185ac
Michael D. Hoyle <hoylemd@gmail.com>
2016-04-25 15:31:44 -0400

    Implement helper and happy path test

--- sfarmy/polls/tests/test_question_view.py
+++ sfarmy/polls/tests/test_question_view.py
@@ -1,9 +1,15 @@
+import datetime
+
 # from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.utils import timezone
+
+from ..models import Question
 
 
-def create_question():
-    pass
+def create_question(text="What's up?", offset=0):
+    date = timezone.now + datetime.timedelta(days=offset)
+    return Question.objects.create(question_text=text, pub_date=date)
 
 
 class QuestionViewTests(TestCase):
@@ -12,6 +18,18 @@ class QuestionViewTests(TestCase):
 
     def test_index_view__future_question(self):
         pass
+
+    def test_index_view__recent_question(self):
+        """
+        Recent (last 24 hours) questions should show up in the context
+        """
+        question = create_question("When did you poop recently?")
+        response = self.client.get(reverse('polls:index'))
+        import ipdb; ipdb.set_trace()
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
 
     def test_index_view__past_question(self):
         pass
