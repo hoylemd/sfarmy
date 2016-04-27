98ab7e438758b6ad68d33acd31f718e38d91edae
Michael D. Hoyle <hoylemd@gmail.com>
2016-04-27 13:00:39 -0400

    Add simple function-based signup

--- sfarmy/base/urls.py
+++ sfarmy/base/urls.py
@@ -4,10 +4,18 @@ from django.contrib.auth.views import login, logout
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 
+from .views import register, registration_complete
+
+app_name = 'base'
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', login),
-    url(r'^logout/$', logout),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+
+    # Registration URLs
+    url(r'^register/$', register, name='register'),
+    url(r'^register/complete/$', registration_complete,
+        name='registration_complete'),
 ] + staticfiles_urlpatterns()
--- sfarmy/base/views.py
+++ sfarmy/base/views.py
@@ -1,4 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.core.context_processors import csrf
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
 from django.utils.decorators import method_decorator
 
 
@@ -7,3 +12,23 @@ class LoggedInMixin(object):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(LoggedInMixin, self).dispatch(*args, **kwargs)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('registration_complete'))
+
+    else:
+        form = UserCreationForm()
+    token = {}
+    token.update(csrf(request))
+    token['form'] = form
+
+    return render_to_response('registration/registration_form.html', token)
+
+
+def registration_complete(request):
+    return render_to_response('registration/registration_complete.html')
