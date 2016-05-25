50fcd60424d377218e7879af618291de4ecc33be
Michael D. Hoyle <hoylemd@gmail.com>
2016-05-25 13:38:14 -0400

    fix ability model

--- app/hs_sim/models/ability.py
+++ app/hs_sim/models/ability.py
@@ -2,4 +2,6 @@ from django.db import models
 
 
 class Ability(models.Model):
+    name = models.CharField(max_length=200)
+    text = models.CharField(max_length=200)
     slug = models.fields.SlugField()
