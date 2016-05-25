e870592e5a257600a219e83138475457b4dd01a5
Michael D. Hoyle <hoylemd@gmail.com>
2016-05-25 13:45:34 -0400

    Add ability values

--- app/hs_sim/models/ability.py
+++ app/hs_sim/models/ability.py
@@ -5,6 +5,11 @@ class Ability(models.Model):
     name = models.CharField(max_length=200)
     text = models.CharField(max_length=200)
     slug = models.fields.SlugField()
+    value_spec = models.CharField(max_length=200)
+
+    @property
+    def value(self):
+        return self.value_spec
 
     def __str__(self):
         return self.name
